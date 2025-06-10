7a33cf01e73a00b6b0962d2e20364ee099807f90
chuh007 <chhuh007@gmail.com>
2025-06-10 12:48:05 +0900

    fix : GameObject, ObjectManager

--- GameP_TeamMakeGame/ObjectManager.cpp
+++ GameP_TeamMakeGame/ObjectManager.cpp
@@ -1,11 +1,35 @@
 #include "ObjectManager.h"
+#include "GameObject.h"
 ObjectManager* ObjectManager::m_inst = __nullptr;
+ObjectManager::ObjectManager()
+{
 
+}
 ObjectManager::~ObjectManager()
 {
 	for (auto& e : m_objects)
 	{
 		delete e;
 	}
+}
+
+void ObjectManager::Update()
+{
+	for (int i = 0;i < m_objects.size();++i)
+	{
+		if (m_objects[i]->isDead)
+		{
+			delete m_objects[i];
+			m_objects.erase(m_objects.begin() + i);
+			continue;
+		}
+		m_objects[i]->Update();
+		m_objects[i]->Render();
+	}
+}
+
+void ObjectManager::AddObject(GameObject* obj)
+{
+	m_objects.push_back(obj);
 }
 
--- GameP_TeamMakeGame/ObjectManager.h
+++ GameP_TeamMakeGame/ObjectManager.h
@@ -2,6 +2,9 @@
 #include "defines.h"
 #include "GameObject.h"
 #include <vector>
+
+class GameObject;
+
 class ObjectManager
 {
 private:
@@ -18,24 +21,8 @@ public:
 	{
 		SAFE_DELETE(m_inst);
 	}
-	void Update()
-	{
-		for (int i = 0;i < m_objects.size();++i)
-		{
-			if (m_objects[i]->isDead)
-			{
-				delete m_objects[i];
-				m_objects.erase(m_objects.begin() + i);
-				continue;
-			}
-			m_objects[i]->Update();
-			m_objects[i]->Render();
-		}
-	}
-	void AddObject(GameObject* obj)
-	{
-		m_objects.push_back(obj);
-	}
+	void Update();
+	void AddObject(GameObject* obj);
 private:
 	static ObjectManager* m_inst;
 	std::vector<GameObject*> m_objects;
