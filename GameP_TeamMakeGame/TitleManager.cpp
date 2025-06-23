d763bf00bb1d5dc8ed4ebf1277066658c63b8a91
moon080411 <anstjdalsdlek@gmail.com>
2025-06-23 21:30:57 +0900

    feat:TitleManager

---
+++ GameP_TeamMakeGame/TitleManager.cpp
@@ -0,0 +1,46 @@
+﻿#include<io.h>
+#include "TitleManager.h"
+#include "Console.h"
+#include "Position.h"
+
+TitleManager* TitleManager::m_inst = __nullptr;
+
+TitleManager::TitleManager()
+	:eMenu(Menu::FAIL)
+{
+	eMenu = Menu::START;
+}
+
+void TitleManager::Update(Key key)
+{
+	
+
+	Render();
+}
+
+void TitleManager::Render()
+{
+	Position resolution = GetConsoleResolution();
+	int y = resolution.y / 3;
+	IsGotoxy(0, y);
+	int coutMode = _setmode(_fileno(stdout), _O_U16TEXT);
+	wcout << L" \t \t ██╗     ██╗ ██████╗ ██╗  ██╗████████╗    ██╗  ██╗███████╗██████╗  ██████╗  " << '\n';
+	wcout << L" \t \t ██║     ██║██╔════╝ ██║  ██║╚══██╔══╝    ██║  ██║██╔════╝██╔══██╗██╔═══██╗ " << '\n';
+	wcout << L" \t \t ██║     ██║██║  ███╗███████║   ██║       ███████║█████╗  ██████╔╝██║   ██║ " << '\n';
+	wcout << L" \t \t ██║     ██║██║   ██║██╔══██║   ██║       ██╔══██║██╔══╝  ██╔══██╗██║   ██║ " << '\n';
+	wcout << L" \t \t ███████╗██║╚██████╔╝██║  ██║   ██║       ██║  ██║███████╗██║  ██║╚██████╔╝ " << '\n';
+	wcout << L" \t \t ╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝  " << '\n';
+	int wcoutMode = _setmode(_fileno(stdout), coutMode);
+
+	int x = resolution.x / 2.5f;
+	y = resolution.y / 3 * 2;
+	IsGotoxy(x, y);
+	cout << "게임 시작";
+	IsGotoxy(x, y + 1);
+	cout << "게임 정보";
+	IsGotoxy(x, y + 2);
+	cout << "게임 종료";
+
+	IsGotoxy(x - 1, y + (int)eMenu);
+	cout << ">";
+}
---
+++ GameP_TeamMakeGame/TitleManager.h
@@ -0,0 +1,34 @@
+#pragma once
+#include"Enums.h"
+#include "Define.h"
+#include "ICommand.h"
+#include <vector>
+
+class TitleManager
+{
+public:
+	TitleManager();
+
+public:
+	static TitleManager* GetInst()
+	{
+		if (m_inst == nullptr)
+			m_inst = new TitleManager;
+		return m_inst;
+	}
+	static void DestoryInst()
+	{
+		SAFE_DELETE(m_inst);
+	}
+private:
+	static TitleManager* m_inst;
+
+public:
+	void Update(Key key);
+	Menu GetTitleState() { return eMenu; }
+private:
+	void Render();
+private:
+	Menu eMenu;
+};
+
